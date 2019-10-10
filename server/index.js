c0ddce3c1e3f84d77f8d4e5b7129f2c87c01d7bf
jc_ohladkov <ohladkov@justcoded.co>
2019-10-10 10:53:22 +0300

    split client and server packages

--- server/index.js
+++ server/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const multer = require('multer');
 
 const PORT = 8081;
+const UPLOAD_DIR = './uploads/';
 
-const upload = multer({ dest: './uploads/' });
+const storage = multer.diskStorage({
+  destination(req, res, cb) {
+    cb(null, UPLOAD_DIR);
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
 const app = express();
 
 app.use(express.json());
